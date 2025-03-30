52cf0d6ba8ef461dad60ae04f96766190b4625a7
Elie-Dev250 <iradukundaelie71@gmail.com>
2025-03-31 00:04:01 +0200

    digital clock

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+
 
 function Home() {
-  return (
-    <div>
-      <h1>Home page</h1>
+
+  const [time,setTime]=useState(new Date())
+  useEffect(()=>{
+    const interval=setInterval(()=>{
+setTime(new Date());
+    },1000)
+
+
+    return ()=>{
+      clearInterval(interval);
+    }
+  },[]);
+
+
+    function format(){
+      let hours=time.getHours()
+      const minutes=time.getMinutes()
+      const seconds=time.getSeconds()
+      const meridium=hours>=12? 'PM' :'AM';
+      hours=hours%12 || 12
+
+      return`${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}:${meridium}`
+
+      
+    }
+
+    function padZero(numbers){
+      return (numbers < 10 ? "0": "") +numbers
+    }
+  
+return(
+  <div className='digital'>
+    <div className='clock'>
+      <span>{format()}</span>
     </div>
-  )
+  </div>
+)
+
 }
 
 export default Home
